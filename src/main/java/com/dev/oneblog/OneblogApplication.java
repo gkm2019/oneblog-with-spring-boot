abbf1d446eced1b179e46636ba011e18868d0e85
eungjlee <eungjlee@smilegate.com>
2021-11-01 23:16:22 +0900

    Initial commit - create project

---
+++ src/main/java/com/dev/oneblog/OneblogApplication.java
@@ -0,0 +1,13 @@
+package com.dev.oneblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneblogApplication.class, args);
+	}
+
+}
