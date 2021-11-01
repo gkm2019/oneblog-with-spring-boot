3547e717e9ef6d6eec9bca7ee4dedb86fde31730
kyeongmin <popo2019@naver.com>
2021-11-01 23:33:23 +0900

    Test hello world (#1)

--- src/main/java/com/dev/oneblog/OneblogApplication.java
+++ src/main/java/com/dev/oneblog/OneblogApplication.java
@@ -8,6 +8,6 @@ public class OneblogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OneblogApplication.class, args);
+		System.out.println("hello world!");
 	}
-
 }
