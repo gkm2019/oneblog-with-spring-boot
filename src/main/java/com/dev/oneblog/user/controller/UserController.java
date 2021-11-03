520b46b22b146863f5127eae536318858a4ac3a2
kyeongmin <popo2019@naver.com>
2021-11-03 22:31:44 +0900

    시큐리티 기본 controller (#2)

--- src/main/java/com/dev/oneblog/user/controller/UserController.java
+++ src/main/java/com/dev/oneblog/user/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 /**
  * Created by kyeongmin ku
  */
@@ -19,8 +21,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("/index")
-    public String index(Model model) {
-        model.addAttribute("message", "hello spring security");
+    public String index(Model model, Principal principal) {
+        if(principal == null) {
+            model.addAttribute("message", "당신은 비회원입니다.");
+        } else {
+            model.addAttribute("message", "userNmae: " + principal.getName());
+        }
+
         return "index";
+    }
+
+    @GetMapping("/info")
+    public String info(Model model) {
+        model.addAttribute("message", "info: 누구나 접근");
+        return "info";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
+        model.addAttribute("message", "userName: " + principal.getName());
+        return "dashboard";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal) {
+        model.addAttribute("message", "admin: " + principal.getName());
+        return "admin";
     }
 }
